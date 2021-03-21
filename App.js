6edfc6a0d8e07b3b39f85e14d13a98b325752f9f
nghoa <ngyshadi@gmail.com>
2021-03-21 18:54:15 +0200

    another try

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {HelloWorld} from './src/components';
+import { HelloWorld } from './src/components';
+import { LoginScreen } from './src/screens/login'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello ...!!!</Text>
+      <Text>Hello asd...!!!</Text>
       <HelloWorld name="Hoa"/>
+      <LoginScreen/>
       <StatusBar style="auto" />
     </View>
   );
